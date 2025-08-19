dd204c5baf83147929e220f4b5ac824b32159528
SpencerF718 <194403957+SpencerF718@users.noreply.github.com>
2025-08-18 23:17:02 -0400

    Add transparency

--- init.lua
+++ init.lua
@@ -895,6 +895,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd 'hi Normal guibg=NONE ctermbg=NONE'
     end,
   },
 
